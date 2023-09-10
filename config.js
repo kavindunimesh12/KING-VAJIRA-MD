a6dff6ae40af6c74363e39507f6c3f32421b1dd2
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-09-10 21:54:06 +0530

    👑KING-VAJIRA👑

--- config.js
+++ config.js
@@ -4,19 +4,19 @@ const fs = require('fs-extra')
   
  //═══════[Required Variables]════════\\ 
  global.owner = process.env.OWNER_NUMBER.split(",") 
- global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority" 
+ global.mongodb = process.env.MONGODB_URI || "mongodb+srv://vajira:vajira@cluster0.hzwqrqq.mongodb.net/?retryWrites=true&w=majority" 
  global.port= process.env.PORT || 5000 
  global.email = 'sam@secktor.live' 
- global.github = 'https://github.com/nipuna15' 
- global.location = 'Sri lanka, Monaragala' 
+ global.github = 'https://github.com/vajirabot1' 
+ global.location = 'Sri lanka, Rathnapura' 
  global.gurl = 'https://instagram.com/' // add your username 
  global.sudo = process.env.SUDO || '919628516236' 
  global.devs = '919628516236'; 
  global.website = 'https://github.com/' //wa.me/+91000000000000 
- global.THUMB_IMAGE = process.env.THUMB_IMAGE || '' 
+ global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/fb7993e24f7ecdaecbed5.jpg' 
  module.exports = { 
-   botname:   process.env.BOT_NAME === undefined ? '' : process.env.BOT_NAME, 
-   ownername: process.env.OWNER_NAME === undefined ? '' : process.env.OWNER_NAME,  
+   botname:   process.env.BOT_NAME === undefined ? 'King-Vajira' : process.env.BOT_NAME, 
+   ownername: process.env.OWNER_NAME === undefined ? 'Vajira' : process.env.OWNER_NAME,  
    author:  process.env.PACK_INFO.split(";")[0] === undefined ? '' : process.env.PACK_INFO.split(";")[0], 
    auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS, 
    packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '-' : process.env.PACK_INFO.split(";")[1],
